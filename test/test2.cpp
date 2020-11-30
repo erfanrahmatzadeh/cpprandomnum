dfe01ae34757fc4ede1cc322860463cb1734fb94
Erfan <marston873@gmail.com>
2020-11-30 14:50:18 +0330

    test

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,23 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 
-int main ( ) {
-int n1,n2;
-cin >>n1>>n2;
-if(n1== n2)
- cout <<"equal"<<endl;
-else
- cout <<"not equal"<<endl;
-if(n1!=n2)
- cout <<"not equal"<<endl;
-if(n1>n2)
- { cout <<n1<<"great than";
-  cout <<n2<<endl;}
-if (n1<=n2)
- { cout <<n1<<"less than"<<n2<<endl;
- cout <<n1<<"or equal"<<n2<<endl;}
-else
-{ cout <<n1<<"great than"<<n2<<endl;
- cout <<n1<<"not equal"<<n2<<endl;}
- return 0;
- }
+int main()
+{
+  int n1, n2;
+  cin >> n1 >> n2;
+  if (n1 == n2)
+    cout << "equal" << endl;
+  else
+    cout << "not equal" << endl;
+  if (n1 != n2)
+    cout << "not equal" << endl;
+  if (n1 > n2)
+  {
+    cout << n1 << "great than";
+    cout << n2 << endl;
+  }
+  if (n1 <= n2)
+  {
+    cout << n1 << "less than" << n2 << endl;
+    cout << n1 << "or equal" << n2 << endl;
+  }
+  else
+  {
+    cout << n1 << "great than" << n2 << endl;
+    cout << n1 << "not equal" << n2 << endl;
+  }
+
+  getch();
+  return 0;
+}
