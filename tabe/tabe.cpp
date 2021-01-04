7fae48bce79492eefbc9d4346e9296de1b7948ec
Erfan <marston873@gmail.com>
2021-01-04 08:44:27 +0330

    new

---
+++ tabe/tabe.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int square(int);
+
+int main()
+{
+    for (int x = 1; x <= 10; x++)
+        cout << square(x) << " ";
+    cout << endl;
+
+
+      getch();
+
+    return 0;
+}
+int square(int y)
+{
+    return y * y;
+}
